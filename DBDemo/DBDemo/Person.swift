83680c5167f4c774ae8fe706d398cb266d7a68c0
Bilal Hassan <bilalhassan@bilals-air>
2019-05-23 01:56:30 +1000

    done dusted

---
+++ DBDemo/DBDemo/Person.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+class Person
+{
+    
+    var name: String = ""
+    var age: Int = 0
+    var id: Int = 0
+    
+    init(id:Int, name:String, age:Int)
+    {
+        self.id = id
+        self.name = name
+        self.age = age
+    }
+    
+}
--- DBDemo/DBDemo/ViewController.swift
+++ DBDemo/DBDemo/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
 
+    @IBOutlet weak var personTable: UITableView!
+    
+    let cellReuseIdentifier = "cell"
+    
+    var db:DBHelper = DBHelper()
+    
+    var persons:[Person] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        personTable.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        personTable.delegate = self
+        personTable.dataSource = self
+        
+        
+        db.insert(id: 0, name: "Bilal", age: 24)
+        db.insert(id: 0, name: "Bosh", age: 25)
+        db.insert(id: 0, name: "Thor", age: 23)
+        db.insert(id: 0, name: "Edward", age: 44)
+        db.insert(id: 0, name: "Ronaldo", age: 34)
+        
+        persons = db.read()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
+        
+        cell.textLabel?.text = "Name: " + persons[indexPath.row].name + ", " + "Age: " + String(persons[indexPath.row].age)
+        
+        return cell
+    }
+    
+    
+    
 }
 
